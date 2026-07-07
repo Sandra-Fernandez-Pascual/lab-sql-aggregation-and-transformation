b97b10299781b527f6e1d4cd798c16403c63c5a9
Sandra Fdez Pascual <sandrafdeztauro@gmail.com>
2026-07-07 21:56:39 +0200

    solved 2nd sql lab day 2

--- lab-sql-aggregation-and-transformation.sql
+++ lab-sql-aggregation-and-transformation.sql
@@ -1,3 +1,5 @@
+Use sakila;
+-- Challenge 1
 -- 1. You need to use SQL built-in functions to gain insights relating to the duration of movies:
 -- 1.1 Determine the shortest and longest movie durations and name the values as max_duration and min_duration.
 SELECT 
@@ -48,3 +50,35 @@ SELECT
   IFNULL(rental_duration, 'Not Available') AS rental_duration
 FROM film
 ORDER BY title ASC;
+
+-- Challenge 2
+-- 1. Next, you need to analyze the films in the collection to gain some more insights. Using the film table, determine:
+-- 1.1 The total number of films that have been released.
+SELECT COUNT(*) AS total_films
+FROM film;
+-- 1.2 The number of films for each rating.
+SELECT rating, COUNT(*) AS number_of_films
+FROM film
+GROUP BY rating;
+-- 1.3 The number of films for each rating, sorting the results in descending order of the number of films.
+SELECT rating, COUNT(*) AS number_of_films
+FROM film
+GROUP BY rating
+ORDER BY number_of_films DESC;
+-- 2. Using the film table, determine:
+-- 2.1 The mean film duration for each rating, and sort the results in descending order of the mean duration.
+-- Round off the average lengths to two decimal places. 
+SELECT 
+    rating, 
+    ROUND(AVG(length), 2) AS mean_duration
+FROM film
+GROUP BY rating
+ORDER BY mean_duration DESC;
+-- 2.2 Identify which ratings have a mean duration of over two hours in order to help select films for customers who prefer longer movies.
+SELECT 
+    rating, 
+    ROUND(AVG(length), 1) AS mean_duration
+FROM film
+GROUP BY rating
+HAVING AVG(length) > 120
+ORDER BY mean_duration DESC;
